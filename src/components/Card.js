25a24463043110a13b054c9a41b944674faf1fb9
Benny Solomon <mr.benny.solomon@gmail.com>
2023-03-01 15:05:22 +0200

    ecommerce  ver 2.2

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import '../styles/Card.modules.css';
 import cardImg from '../img/card.png'
 const Card = () => {
   return (
-    <div class="card">
+    <div className="card">
       <img className='img-card' src={cardImg} alt="Avatar"/>
-      <div class="container">
+      <div className="container">
         <h4><b>John Doe</b></h4>
         <p>Architect & Engineer</p>
       </div>
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -14,7 +14,7 @@ const MainNavigation = () => {
           <li><Link to='/'><img src={logo} alt='logo' className='logo'/></Link></li>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/cart'>Shopping Cart</Link></li>
+          <li><Link to='/cart'>Shopping Cart<label className='amount-of-items'>0</label></Link></li>
           <li><Link to='/courses'>Online Courses</Link></li>
           <li><Link to='/products'>Products</Link></li>
           <li><Link to='/about'>About</Link></li>
