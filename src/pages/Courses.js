d24c5589172b2d8d218cc712a2c5f8297b1bdc40
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-04 20:55:21 +0200

    ecommerce ver 4.1

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import '../styles/Courses.modules.css';
 import Card from '../components/Card';
 import {Link} from 'react-router-dom';
@@ -11,7 +11,14 @@ import digitalCourse from '../img/digital-course.jpg';
 const Courses = () => {
 
   const {arrayIds, updateArrayIds} = useGlobalContext();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const imageRef = useRef();
+
   let courses = JSON.parse(localStorage.getItem('courses'));
+
+  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  const isAdmin = (loggedInUser.type === 'admin');
 
   useEffect(() => {
     
@@ -22,6 +29,8 @@ const Courses = () => {
 
   return (
     <div className='courses'>
+      {isAdmin && <h1>Admin Area</h1>}
+      {isAdmin && <div className='admin-container'><input ref={nameRef} type='text' width='100%' placeholder='Product Name'/><input ref={priceRef} type='text' width='100%' placeholder='Product Price'/><input ref={imageRef} type='text' width='100%' placeholder='Product Image Url'/><button className='btn-delete-admin'>DELETE</button><button className='btn-add-admin'>ADD</button><button className='btn-edit-admin'>EDIT</button></div>}
       <h1>Online Courses</h1>
       <div className='courses-container'>
       {courses && arrayIds.map((id) => <Link key={id} to={`/courses/${id}`}><Card 
