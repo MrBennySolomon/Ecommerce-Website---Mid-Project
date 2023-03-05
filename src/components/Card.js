ce89bc7c0868f4c4027dccb49b1bf16bd01bfeed
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-05 09:24:08 +0200

    ecommerce ver 4.3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import '../styles/Card.modules.css';
 
-const Card = ({title, description, price, src}) => {
+const Card = ({name, price, src}) => {
   return (
     <div className="card">
       <img className='img-card' src={src} alt="Avatar"/>
       <div className="container">
-        <h4 className='title'><b>{title}</b></h4>
-        <p>{description}</p>
+        <h4 className='title'><b>{name}</b></h4>
         <p className='price'>{price}</p>
       </div>
     </div>
