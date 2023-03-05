969890111873f25ab6dabbdab2a49c371c30089c
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-05 13:26:39 +0200

    ecommerce digital course fix bugs ver 4.4

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,13 +1,18 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import '../styles/Course.modules.css';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../context/context';
+import dubai from '../mp4/dubai2.mp4';
+import vid from '../mp4/vid.mp4';
+import vid2 from '../mp4/vid2.mp4';
+import vid3 from '../mp4/vid3.mp4';
 
 const Course = () => {
   const [isLoading, setIsLoading] = useState(false);
   const {updateCount, updateTotal} = useGlobalContext();
+  const videoRef = useRef();
 
   const params = useParams();
   const courses = JSON.parse(localStorage.getItem('courses'));
@@ -16,6 +21,7 @@ const Course = () => {
   const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
   const shoppingCarts = JSON.parse(localStorage.getItem('carts'));
   let userCart = [];
+  const isPurchased = loggedInUser?.courses?.length > 0 && loggedInUser?.courses?.find((course) => course.name === selectedCourse.name);
 
   const addToCartHandler = () => {
     setIsLoading(true);
@@ -60,7 +66,26 @@ const Course = () => {
     }
   }
 
-  return (
+  return isPurchased ? 
+  (
+    <div className='course'>
+      <video ref={videoRef} width="60%" height="80%" controls controlsList="nodownload">
+        <source src={dubai} type="video/mp4"/>
+      </video>
+
+      <div className='course-video-items'>
+        {/* {loggedInUser.courses[0].videos.map ((vid) => {
+          return <button key={vid.name} onClick={() => videoRef.current.src = vid.url}>{vid.name}</button>
+        })}         */}
+        <button onClick={() => videoRef.current.src = dubai}>dubai</button>
+        <button onClick={() => videoRef.current.src = vid}>nails art 1</button>
+        <button onClick={() => videoRef.current.src = vid2}>nails art 2</button>
+        <button onClick={() => videoRef.current.src = vid3}>nails art 3</button>
+      </div>
+    </div>
+  )
+  : 
+  (
     <div className='course'>
       <div className='course-img-container'><img src={selectedCourse.imgUrl} width='100%' height='100%'/></div>
       <div className='course-description-container'>
