1206f799d2899bf718951d51e5a35b6d573f0e63
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-07 16:36:32 +0200

    ecommerce go live feature ver 7.2

--- src/MVC/View/Live.js
+++ src/MVC/View/Live.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Live = () => {
   return (
     <>
-      <iframe src="http://localhost:3000/preview/6405bd19cd8175701aac0560/admin" allow="camera;microphone" title="Live Stream" width='100%' height='80%'></iframe>
+      <iframe src="https://live-broadcasting.vercel.app/meeting/6405bd19cd8175701aac0560/admin" allow="camera;microphone" title="Live Stream" width='100%' height='80%'></iframe>
     </>
   )
 }
